7560d038efd787850d04026d57eca4cf525b94f4
Otavio Neves <92130086+otavionvs@users.noreply.github.com>
2025-08-06 16:58:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express'); // Importa o Express
+
+const app = express(); //Cria o servidor
+
+const port = 3000; //Variavel para armazenar a porta
+
+//Para permitir receber json nas requisições
+app.use(express.json());
+
+const usuarios = [
+    { "id": 1, "nome": "Otavio", "idade": 20, "senha": "123" },
+    { "id": 2, "nome": "Admin", "idade": 20, "senha": "1234" }
+]
+
+//request - requisição
+//response - respota
+app.get("/", (request, response) => {
+    response.send("Primeiro servidor DESI - Malwee");
+})
+
+//Buscar todos os usuários
+app.get("/usuarios", (req, res) => {
+    //send -> envia os dados
+    res.send(usuarios);
+})
+
+//Buscar um usuário -> get by id
+app.get("/usuarios/:id", (req, res) => {
+    //params - parametros da requisição (fica na url)
+    const id = parseInt(req.params.id);
+
+    const usuario = usuarios.find(usuario => usuario.id == id);
+
+    if (usuario != null) {
+        res.send(usuario)
+    } else {
+        res.status(404).send("Usuário não encontrado!")
+    }
+})
+
+//Criar um usuário
+app.post("/usuarios", (req, res) => {
+    //body - corpo da requisição
+    const novoUsuario = req.body;
+    novoUsuario.id = usuarios.length + 1;
+    usuarios.push(novoUsuario);
+
+    res.status(201).send(novoUsuario)
+})
+
+//Atualizar um usuário
+app.put("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const novoUsuario = req.body;
+    novoUsuario.id = id;
+    const index = usuarios.findIndex(usuario => usuario.id == id);
+
+    if (index != null) {
+        usuarios[index] = novoUsuario;
+        res.status(204).send(novoUsuario);
+    } else {
+        res.status(404).send("Usuário não encontrado!")
+    }
+
+})
+
+//Deletar um usuário
+app.delete("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = usuarios.findIndex(usuario => usuario.id == id);
+
+    if (index != null) {
+
+
+        
+        usuarios.splice(index, 1);
+        res.status(204).send("Usuário com id:" + id + " removido com sucesso!")
+    } else {
+        res.status(404).send("Usuário não encontrado!")
+    }
+})
+
+app.listen(port, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+})
