25e1e32c37d5a9089096218eb8a065b797ae5cc0
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2025-08-25 16:54:06 -0300

    estrutura de integração frontend com backend

---
+++ Frontend/script.js
@@ -0,0 +1,18 @@
+
+
+fetch("http://localhost:3000/usuarios").then(async res =>{
+    if(!res.ok){
+        throw new Error("Erro ao buscar usuários")
+    }
+
+    return res.json();
+}).then(usuarios=>{
+    const listaUsuarios = document.getElementById("lista-usuarios");
+    usuarios.forEach(usuario => {
+        console.log(usuario.nome);
+        listaUsuarios.innerHTML += `<li class="list-group-item">${usuario.nome}</li>`;
+    });
+})
+.catch(err=>{
+    console.error(err);
+})
