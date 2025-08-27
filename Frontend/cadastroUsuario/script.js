2a0168071a7b57252b6e15da224f321dfacd5163
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2025-08-27 16:50:41 -0300

    post method

---
+++ Frontend/cadastroUsuario/script.js
@@ -0,0 +1,26 @@
+function cadastroUsuario(event) {
+  event.preventDefault(), console.log(event);
+
+  let nome = document.getElementById("nome").value;
+  let idade = document.getElementById("idade").value;
+  let senha = document.getElementById("senha").value;
+
+  fetch("http://localhost:3000/usuarios", {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      "nome": nome,
+      "idade": idade,
+      "senha": senha
+    }),
+  })
+    .then((response) => response.json())
+
+    .then((data) => console.log(data))
+
+    .catch((error) => console.log(error));
+}
